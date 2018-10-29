2b1f1453ca2c9660e8a78a020b66a20a238ee7bd
ChocoS <chocos@tlen.pl>
2018-10-30 00:07:09 +0100

    Updated to Spring Boot 2

--- server/src/main/java/com/pwawrzyniak/tlog/server/controller/BillController.java
+++ server/src/main/java/com/pwawrzyniak/tlog/server/controller/BillController.java
@@ -5,7 +5,6 @@ import com.pwawrzyniak.tlog.model.BillItem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
